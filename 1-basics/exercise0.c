117c3d28478e507b8c7bbb3e96a8311fc2a1cbda
HandyArchTech <liquorash67@gmail.com>
2025-12-10 03:15:31 -0500

    Update exercise0.c

--- 1-basics/exercise0.c
+++ 1-basics/exercise0.c
@@ -1,3 +1,7 @@
 #include "exercise0.h"
 
-float get_average(int x, int y, int z) { return (float)(x + y + z) / 3; }
+float get_average(int x, int y, int z) { 
+  
+  return (float)(x + y + z) / 3; 
+
+}
